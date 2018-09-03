16bb95fd4ca86f92cf7a5e56b59c2e7581ccf70d
Yunjun Ma <yunjunma@gmail.com>
2018-09-03 14:03:20 -0700

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NarBar from '../../features/nav/NarBar/NavBar';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1 >Welcome to React</h1>
+        <NarBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
       </div>
     );
   }
---
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import { Grid, Button } from 'semantic-ui-react'
+import EventList from '../EventList/EventList'
+import EventForm from '../EventForm/EventForm'
+
+// s_3.7.1
+const eventsDashboard = [
+  {
+    id: '1',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27T11:00:00+00:00',
+    category: 'culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      },
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: 'Trip to Punch and Judy Pub',
+    date: '2018-03-28T14:00:00+00:00',
+    category: 'drinks',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+    attendees: [
+      {
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      },
+      {
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+      }
+    ]
+  }
+]
+
+
+class EventDashboard extends Component {
+  // constructor(props){
+  //   super(props)
+
+  //   this.state = {
+  //     events: eventsDashboard,
+  //     isOpen: false
+  //   }
+
+  //   // handleFormOpen bind in to the Component class but it looks bad
+  //   // this.handleFormOpen = this.handleFormOpen.bind(this);
+  //   // this.handleCancel = this.handleCancel.bind(this);
+  // }
+
+  state = {
+    events: eventsDashboard,
+    isOpen: false
+  }
+
+  // update to arrow function, so we don't need bind below function to instructor
+  handleFormOpen = () => {
+    this.setState({
+      isOpen: true
+    })
+  }
+
+  handleCancel= () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList events={this.state.events} />
+        </Grid.Column>
+        <Grid.Column width={6}>
+        {/* {this.handleFormOpen.hind(this)} this will create a new function everytime, too expensive!! */}
+        {/* () => this.handleFormOpen() is same */}
+        {/* onClick={this.handleFormOpen}, old one */}
+          <Button onClick={this.handleFormOpen} positive content='Create Event'/>
+          {this.state.isOpen && 
+           <EventForm handleCancel={this.handleCancel} />}
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+
+export default EventDashboard
