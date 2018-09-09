e4f001832690915c78d5ebe52946b91cb8cdd4af
Yunjun Ma <yunjunma@gmail.com>
2018-09-09 11:06:51 -0700

    End of section 8

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,62 +1,19 @@
 import React, { Component } from 'react';
+// high order component
+import { connect } from 'react-redux'
 import { Grid, Button } from 'semantic-ui-react';
-import cuid from 'cuid';
 import EventList from '../EventList/EventList';
-import EventForm from '../EventForm/EventForm';
+import { deleteEvent } from '../eventActions'
 
+const mapState = (state) => ({
+  events: state.events
+})
 
-// s_3.7.1
-const eventsDashboard = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
-
+const actions = {
+  // createEvent,
+  deleteEvent,
+  // updateEvent
+}
 
 class EventDashboard extends Component {
   // constructor(props){
@@ -72,89 +29,96 @@ class EventDashboard extends Component {
   //   // this.handleCancel = this.handleCancel.bind(this);
   // }
 
-  state = {
-    events: eventsDashboard,
-    isOpen: false,
-    selectedEvent: null
-  }
+  // state = {
+  //   isOpen: false,
+  //   selectedEvent: null
+  // }
 
   // update to arrow function, so we don't need bind below function to instructor
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null,
-      isOpen: true
-    })
-  }
+  // handleFormOpen = () => {
+  //   this.setState({
+  //     selectedEvent: null,
+  //     isOpen: true
+  //   })
+  // }
 
-  handleCancel= () => {
-    this.setState({
-      isOpen: false
-    })
-  }
+  // handleCancel = () => {
+  //   this.setState({
+  //     isOpen: false
+  //   })
+  // }
 
-  handleCreateEvent = (newEvent) => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL= '/assets/images/user.png';
-    // "..." take the events array and split them out
-    const updatedEvents = [...this.state.events, newEvent];
-    this.setState({
-      events: updatedEvents,
-      isOpen: false
-      
-    })
-  }
+  // handleCreateEvent = newEvent => {
+  //   newEvent.id = cuid();
+  //   newEvent.hostPhotoURL = '/assets/images/user.png';
+  //   // "..." take the events array and split them out
+  //   // const updatedEvents = [...this.state.events, newEvent];
+  //   this.props.createEvent(newEvent);
+  //   this.setState({
+  //     // events: updatedEvents,
+  //     isOpen: false
+  //   })
+  // }
 
-  handleUpdateEvent = ( updatedEvent) => {
-    this.setState({
-      events: this.state.events.map(event =>{
-        if (event.id === updatedEvent.id){
-          // same as clone an object
-          return Object.assign({}, updatedEvent)
-        } else {
-          // the exsiting event
-          return event
-        }
-      }),
-      isOpen: false,
-      selectedEvent: null
-    })
-  }
+  // handleUpdateEvent = updatedEvent => {
+  //   this.props.updateEvent(updatedEvent)
+  //   this.setState({
+  //     // events: this.state.events.map(event =>{
+  //     //   if (event.id === updatedEvent.id){
+  //     //     // same as clone an object
+  //     //     return Object.assign({}, updatedEvent)
+  //     //   } else {
+  //     //     // the exsiting event
+  //     //     return event
+  //     //   }
+  //     // }),
+  //     isOpen: false,
+  //     selectedEvent: null
+  //   })
+  // }
 
   //need to get the event from eventListItem so use the arrow function
-  handleOpenEvent = (eventToOpen) => () => {
-    this.setState({
-      selectedEvent: eventToOpen,
-      isOpen: true
-    })
-  }
+  // handleOpenEvent = eventToOpen => () => {
+  //   this.setState({
+  //     selectedEvent: eventToOpen,
+  //     isOpen: true
+  //   })
+  // }
 
-  handleDeleteEvent = (eventId) => () => {
-    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
-    this.setState({
-      events: updatedEvents
-    })
+  handleDeleteEvent = eventId => () => {
+    // const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    // this.setState({
+    //   events: updatedEvents
+    // })
+    this.props.deleteEvent(eventId);
   }
 
   render() {
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "{...}" below, do not miss it
-    const {selectedEvent} = this.state;
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "{ }" below, do not miss it
+    // const { selectedEvent } = this.state;
+    const { events } = this.props;
+
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events} />
+          <EventList
+            deleteEvent={this.handleDeleteEvent}
+            // onEventOpen={this.handleOpenEvent}
+            events={events}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
-        {/* {this.handleFormOpen.hind(this)} this will create a new function everytime, too expensive!! */}
-        {/* () => this.handleFormOpen() is same */}
-        {/* onClick={this.handleFormOpen}, old one */}
-          <Button onClick={this.handleFormOpen} positive content='Create Event'/>
+          {/* {this.handleFormOpen.hind(this)} this will create a new function everytime, too expensive!! */}
+          {/* () => this.handleFormOpen() is same */}
+          {/* onClick={this.handleFormOpen}, old one */}
+          {/* <Button onClick={this.handleFormOpen} positive content='Create Event' />
           {this.state.isOpen && (
             <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />
-          )}
+          )} */}
         </Grid.Column>
       </Grid>
     )
   }
 }
 
-export default EventDashboard
+export default connect(mapState, actions)(EventDashboard);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
-import { configureStore } from './app/store/configureStore'
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 
@@ -16,7 +17,9 @@ let render = () => {
     ReactDOM.render(
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <ScrollToTop>
+                    <App />
+                </ScrollToTop>
             </BrowserRouter>
         </Provider>,
         rootEl
